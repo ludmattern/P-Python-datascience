8450c8ded7db8068f887ca8db7d23de62d2001b9
ludmattern <github.rfgrg@passmail.net>
2025-06-02 12:33:15 +0200

    feat: added all_thing_is_obj and tester.py

---
+++ Python - 0 - Starting/ex02/find_ft_type.py
@@ -0,0 +1,26 @@
+def all_thing_is_obj(object: any) -> int:
+    """
+    Function that prints the object type and returns 42.
+
+    Args:
+        object: any type of object to analyze
+
+    Returns:
+        int: always returns 42
+    """
+    obj_type = type(object)
+
+    if obj_type is list:
+        print(f"List : {obj_type}")
+    elif obj_type is tuple:
+        print(f"Tuple : {obj_type}")
+    elif obj_type is set:
+        print(f"Set : {obj_type}")
+    elif obj_type is dict:
+        print(f"Dict : {obj_type}")
+    elif obj_type is str:
+        print(f"{object} is in the kitchen : {obj_type}")
+    else:
+        print("Type not found")
+
+    return 42
